ac5b258836e3b45a51cf912b01bc6b1078a4fca3
maryamboneh <maryamboneh@gmail.com>
2022-08-01 17:26:15 +0430

    admin panel - initial

--- resources/views/admin_template.blade.php
+++ resources/views/admin_template.blade.php
@@ -1,38 +1,218 @@
 <!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <title>@yield('title')</title>
-        <meta content="width=device-width, initial-scale=1.0" name="viewport">
-        <meta content="Free HTML Templates" name="keywords">
-        <meta content="Free HTML Templates" name="description">
-        <link href="{{ asset('img/favicon.ico')}}" rel="icon">
-        <link rel="preconnect" href="https://fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">  
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
-        <link href="{{ asset('lib/animate/animate.min.css')}}" rel="stylesheet">
-        <link href="{{ asset('lib/owlcarousel/assets/owl.carousel.min.css')}}" rel="stylesheet">
-        <link href="{{ asset('css/style.css')}}" rel="stylesheet">
-    </head>
+<html lang="ar" dir="rtl">
 
-    <body dir="rtl">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('img/apple-icon.png')}}">
+  <link rel="icon" type="image/png" href="{{ asset('img/favicon.ico')}}">
+  <title>
+  @yield('title')
+  </title>
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+  <link href="{{ asset('css/nucleo-icons.css')}}" rel="stylesheet" />
+  <link href="{{ asset('css/nucleo-svg.css')}}" rel="stylesheet" />
+  <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+  <link id="pagestyle" href="{{ asset('css/soft-ui-dashboard.css?v=1.0.3')}}" rel="stylesheet" />
+</head>
 
-        @if ($session("message"))
-        <div class="alert alert-{{ $session('message_type') }}" role="alert">
-            {{ $session("message") }}
-        </div>
-        @endif
+    <body class="g-sidenav-show rtl bg-gray-100">
+
 
         @yield('content')
 
 
+    <!--   Core JS Files   -->
+    <script src="{{ url('js/core/popper.min.js')}}"></script>
+    <script src="{{ url('js/core/bootstrap.min.js')}}"></script>
+    <script src="{{ url('js/plugins/perfect-scrollbar.min.js')}}"></script>
+    <script src="{{ url('js/plugins/smooth-scrollbar.min.js')}}"></script>
+    <script src="{{ url('js/plugins/fullcalendar.min.js')}}"></script>
+    <script src="{{ url('js/plugins/chartjs.min.js')}}"></script>
+    <script>
+        var ctx = document.getElementById("chart-bars").getContext("2d");
 
-        <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
-        <script src="{{ url('lib/easing/easing.min.js')}}"></script>
-        <script src="{{ url('lib/owlcarousel/owl.carousel.min.js')}}"></script>
-        <script src="{{ url('mail/jqBootstrapValidation.min.js')}}"></script>
-        <script src="{{ url('mail/contact.js')}}"></script>
-        <script src="{{ url('js/main.js')}}"></script>
+        new Chart(ctx, {
+        type: "bar",
+        data: {
+            labels: ["Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+            datasets: [{
+            label: "Sales",
+            tension: 0.4,
+            borderWidth: 0,
+            borderRadius: 4,
+            borderSkipped: false,
+            backgroundColor: "#fff",
+            data: [450, 200, 100, 220, 500, 100, 400, 230, 500],
+            maxBarThickness: 6
+            }, ],
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            plugins: {
+            legend: {
+                display: false,
+            }
+            },
+            interaction: {
+            intersect: false,
+            mode: 'index',
+            },
+            scales: {
+            y: {
+                grid: {
+                drawBorder: false,
+                display: false,
+                drawOnChartArea: false,
+                drawTicks: false,
+                },
+                ticks: {
+                suggestedMin: 0,
+                suggestedMax: 500,
+                beginAtZero: true,
+                padding: 15,
+                font: {
+                    size: 14,
+                    family: "Open Sans",
+                    style: 'normal',
+                    lineHeight: 2
+                },
+                color: "#fff"
+                },
+            },
+            x: {
+                grid: {
+                drawBorder: false,
+                display: false,
+                drawOnChartArea: false,
+                drawTicks: false
+                },
+                ticks: {
+                display: false
+                },
+            },
+            },
+        },
+        });
+
+
+        var ctx2 = document.getElementById("chart-line").getContext("2d");
+
+        var gradientStroke1 = ctx2.createLinearGradient(0, 230, 0, 50);
+
+        gradientStroke1.addColorStop(1, 'rgba(203,12,159,0.2)');
+        gradientStroke1.addColorStop(0.2, 'rgba(72,72,176,0.0)');
+        gradientStroke1.addColorStop(0, 'rgba(203,12,159,0)'); //purple colors
+
+        var gradientStroke2 = ctx2.createLinearGradient(0, 230, 0, 50);
+
+        gradientStroke2.addColorStop(1, 'rgba(20,23,39,0.2)');
+        gradientStroke2.addColorStop(0.2, 'rgba(72,72,176,0.0)');
+        gradientStroke2.addColorStop(0, 'rgba(20,23,39,0)'); //purple colors
+
+        new Chart(ctx2, {
+        type: "line",
+        data: {
+            labels: ["Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+            datasets: [{
+                label: "Mobile apps",
+                tension: 0.4,
+                borderWidth: 0,
+                pointRadius: 0,
+                borderColor: "#cb0c9f",
+                borderWidth: 3,
+                backgroundColor: gradientStroke1,
+                fill: true,
+                data: [50, 40, 300, 220, 500, 250, 400, 230, 500],
+                maxBarThickness: 6
+
+            },
+            {
+                label: "Websites",
+                tension: 0.4,
+                borderWidth: 0,
+                pointRadius: 0,
+                borderColor: "#3A416F",
+                borderWidth: 3,
+                backgroundColor: gradientStroke2,
+                fill: true,
+                data: [30, 90, 40, 140, 290, 290, 340, 230, 400],
+                maxBarThickness: 6
+            },
+            ],
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            plugins: {
+            legend: {
+                display: false,
+            }
+            },
+            interaction: {
+            intersect: false,
+            mode: 'index',
+            },
+            scales: {
+            y: {
+                grid: {
+                drawBorder: false,
+                display: true,
+                drawOnChartArea: true,
+                drawTicks: false,
+                borderDash: [5, 5]
+                },
+                ticks: {
+                display: true,
+                padding: 10,
+                color: '#b2b9bf',
+                font: {
+                    size: 11,
+                    family: "Open Sans",
+                    style: 'normal',
+                    lineHeight: 2
+                },
+                }
+            },
+            x: {
+                grid: {
+                drawBorder: false,
+                display: false,
+                drawOnChartArea: false,
+                drawTicks: false,
+                borderDash: [5, 5]
+                },
+                ticks: {
+                display: true,
+                color: '#b2b9bf',
+                padding: 20,
+                font: {
+                    size: 11,
+                    family: "Open Sans",
+                    style: 'normal',
+                    lineHeight: 2
+                },
+                }
+            },
+            },
+        },
+        });
+    </script>
+    <script src="{{ url('js/plugins/choices.min.js')}}"></script>
+    <script>
+        var win = navigator.platform.indexOf('Win') > -1;
+        if (win && document.querySelector('#sidenav-scrollbar')) {
+        var options = {
+            damping: '0.5'
+        }
+        Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
+        }
+    </script>
+    <!-- Github buttons -->
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
+    <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
+    <script src="{{ url('js/soft-ui-dashboard.min.js?v=1.0.3')}}"></script>
     </body>
+
 </html>
