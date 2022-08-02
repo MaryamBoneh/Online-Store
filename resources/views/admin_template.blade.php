da3be5eee65ea7ade2955bb564c7ac14e24024d0
maryamboneh <maryamboneh@gmail.com>
2022-08-02 15:56:01 +0430

    setting and sidebar pages

--- resources/views/admin_template.blade.php
+++ resources/views/admin_template.blade.php
@@ -9,7 +9,6 @@
   <title>
   @yield('title')
   </title>
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
   <link href="{{ asset('css/nucleo-icons.css')}}" rel="stylesheet" />
   <link href="{{ asset('css/nucleo-svg.css')}}" rel="stylesheet" />
   <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
